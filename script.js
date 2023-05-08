33ffb2dbab5ce13ee9fbf317adc9f747c7f8752d
marcela-celani <marcelacelani.ti@gmail.com>
2023-05-08 16:04:50 -0300

    Projeto finalizado primeira entrega

--- script.js
+++ script.js
@@ -4,9 +4,30 @@ clickMenu = () => {
     menu.classList.toggle("active")
 }
 
+// LOGICA DOS BUTTONS
+
+// ao passar o mouse
+hoverButton = (event) => {
+    let button = event.target
+    button.classList.add('class', 'buttonHover')
+}
+
+// ao retirar o mouse
+unhoverButton = (event) => {
+    let button = event.target
+    button.classList.remove('class', 'buttonHover') 
+}
+
+// ao clicar com o mouse
+clickButton = (event) => {
+    let button = event.target
+    button.classList.add('class', 'buttonClick')
+}
+
 //VALIDAÇÃO CADASTRO EMAIL
 const form = document.getElementById('flex-cadastro')
 const emailImput = document.getElementById('cadastro')
+
 
 form.addEventListener("submit", (event) => {
 
@@ -19,15 +40,16 @@ form.addEventListener("submit", (event) => {
     form.submit()
 })
 
+
 function emailValidacao(email) {
     const emailRegex = new RegExp(
         // usuario123@host.com
-        /^[a-zA-Z0-9._-]+@+[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+        /^[a-zA-Z0-9._-]+@+[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/)
+        
 
         if(emailRegex.test(email)) {
             return true
         } else {
             return false
         }
-    )
 }
